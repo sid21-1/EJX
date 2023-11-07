c582fa8eace9725f95fb9bc32f3657d4f9eb61b5
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-07 08:53:27 +0530

    learning EJX

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = 8080;
+
+// Important lines to not get errors
+app.set("view engine", "ejs");
+app.set("views,", path.join(__dirname, "/views"));
+
+app.listen(port, () => {
+  console.log(`app is listening on port ${port} `);
+});
+
+app.get("/ig/:username",(req,res)=>{
+    const followers =["adam", "bob", "aman"]
+    let {username}= req.params;
+    res.render("instagram.ejs" ,{username, followers})
+})
+
+app.get("/rollDice", (req, res) => {
+  let diceVal = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice.ejs", {diceVal});
+});
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
