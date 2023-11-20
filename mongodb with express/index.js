d23d96c2bcdf893091807dc8b1f31782524474b3
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-20 11:58:04 +0530

    created edit  route

--- mongodb with express/index.js
+++ mongodb with express/index.js
@@ -51,6 +51,13 @@ app.post("/chats", (req, res) => {
   res.redirect("/chats");
 });
 
+// Edit route
+app.get("/chats/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs", { chat });
+});
+
 app.get("/", (req, res) => {
   res.send("root is working");
 });
