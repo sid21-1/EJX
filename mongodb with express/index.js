00951f4da8ebc863326f4e0b985aa158ac95b390
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-20 08:35:28 +0530

    done making all the connections

--- mongodb with express/index.js
+++ mongodb with express/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 main()
   .then(() => {
