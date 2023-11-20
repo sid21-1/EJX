b37f2669fc94f186c2b0560f9212f72554458da2
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-20 09:50:54 +0530

    connecting front end and backend together

--- mongodb with express/index.js
+++ mongodb with express/index.js
@@ -6,6 +6,7 @@ const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 main()
   .then(() => {
@@ -16,6 +17,13 @@ main()
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
+
+// index route
+app.get("/chats", async (req, res) => {
+  let chats = await Chat.find();
+  // console.log(chats);
+  res.render("index.ejs", { chats });
+});
 
 app.get("/", (req, res) => {
   res.send("root is working");
