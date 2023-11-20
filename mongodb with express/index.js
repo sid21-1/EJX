b5eac6b7512acd2a39bd401252cc4786e86f1a11
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-20 08:32:50 +0530

    setting up of mongodb done

--- mongodb with express/index.js
+++ mongodb with express/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
-app.get("/", (req,res)=>{
-    res.send("root is working");
-})
+main()
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+app.get("/", (req, res) => {
+  res.send("root is working");
+});
 
 app.listen(8080, () => {
   console.log("server is listening on port 8080");
