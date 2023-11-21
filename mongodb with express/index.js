b852761b3b8614edb878982f0814602ac672c4f4
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-21 08:07:28 +0530

    created the deleted route

--- mongodb with express/index.js
+++ mongodb with express/index.js
@@ -74,6 +74,14 @@ app.put("chats/:id", async (req, res) => {
   res.redirect("/chats");
 });
 
+// Destroy Route
+app.delete("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedChat = await Chat.findByIdAndDelete(id);
+  console.log(deletedChat);
+  res.redirect("/chats");
+});
+
 app.get("/", (req, res) => {
   res.send("root is working");
 });
